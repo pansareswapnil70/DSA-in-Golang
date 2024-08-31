5c9500ed8ed5a271d21937cd0bd15bbbd7c411ad
Swapnil Pansare <pansareswapnil70@gmail.com>
2024-08-31 11:46:23 +0530

    Recursive Reverse Linked list function and comments added

--- Linked lists/main.go
+++ Linked lists/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 )
 
+// struct to define the structure of the node.
 type Node struct {
 	data int
 	next *Node
 }
 
+// struct to define the head of the linkedlist.
 type Linkedlist struct {
 	head   *Node
 	length int
@@ -27,38 +29,58 @@ func main() {
 	ll.deleteAtHead()
 	ll.deleteAtTail()
 	ll.deleteAtPosition(3)
-	ll.deleteAtPosition(2)
-	ll.reverseLinkedList()
+	ll.head = ll.reverseLinkedList()
+	ll.head = ll.recursiveReverseLinkedList(ll.head)
+	fmt.Println("Reversed a linked list using recursive approach:")
+	ll.printList()
 }
 
+// Delete a node at specific position in linked list.
 func (ll *Linkedlist) deleteAtPosition(position int) {
 	temp := ll.head
 	for i := 1; i < position-1; i++ {
 		temp = temp.next
 	}
 	temp.next = temp.next.next
-	fmt.Println("Updated Linked list is:")
+	fmt.Println("Updated Linked list after delete at position:", position)
 	ll.printList()
 
 }
 
+// Delete a node from head / first node of the linked list.
 func (ll *Linkedlist) deleteAtHead() {
 	temp := ll.head
 	ll.head = temp.next
 	fmt.Println("\nDeleted node at head is: ", temp.data)
+	fmt.Println("Updated Linked list after delete at head:")
 	ll.printList()
 }
 
+// Delete a node from tail / last node of the linked list.
 func (ll *Linkedlist) deleteAtTail() {
 	temp := ll.head
 	for temp.next.next != nil {
 		temp = temp.next
 	}
 	fmt.Println("\nDeleted node at tail is: ", temp.next.data)
+	fmt.Println("Updated Linked list after delete at tail:")
 	temp.next = nil
 	ll.printList()
 }
 
+// Reverse a linked list using recursive approach.
+func (ll *Linkedlist) recursiveReverseLinkedList(node *Node) *Node {
+	if node == nil || node.next == nil {
+		return node
+	}
+	newNode := ll.recursiveReverseLinkedList(node.next)
+	node.next.next = node
+	node.next = nil
+
+	return newNode
+}
+
+// Reverse a linked list using Iterative approach.
 func (ll *Linkedlist) reverseLinkedList() *Node {
 	var prev *Node
 	curr := ll.head
@@ -69,12 +91,13 @@ func (ll *Linkedlist) reverseLinkedList() *Node {
 		curr = next
 	}
 	ll.head = prev
-	fmt.Println("Reversed a linked list:")
+	fmt.Println("Reversed a linked list using iterative approach:")
 	ll.printList()
 	return prev
 
 }
 
+// Print the linked list.
 func (ll *Linkedlist) printList() {
 	current := ll.head
 	for current != nil {
@@ -88,6 +111,7 @@ func (ll *Linkedlist) printList() {
 	fmt.Println()
 }
 
+// Insert a node at specific position in linked list.
 func (ll *Linkedlist) insertAtPosition(data int, position int) {
 	var node = &Node{data, nil}
 	temp := ll.head
@@ -102,6 +126,7 @@ func (ll *Linkedlist) insertAtPosition(data int, position int) {
 	ll.printList()
 }
 
+// Insert a node at head / first position in linked list.
 func (ll *Linkedlist) insertAtHead(data int) {
 	node := &Node{data, nil}
 	if ll.head == nil {
@@ -116,6 +141,7 @@ func (ll *Linkedlist) insertAtHead(data int) {
 	ll.printList()
 }
 
+// Insert a node at tail / last position in linked list.
 func (ll *Linkedlist) insertAtTail(data int) {
 	node := &Node{data, nil}
 	temp := ll.head
